6a89afc87b712651f51ffb60c527a8c075f3ac84
Fernando <fernando.luiz92@hotmail.com>
2022-01-23 16:39:03 -0300

    Refatorado a instanciação

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -63,18 +63,8 @@ public class SellerDaoJDBC implements SellerDao {
 			// Se minha consulta não retornou nenhum registro, meu rs vai dar falso, pular o if
 			// e vai retorna null
 			if(rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				
-				Seller seller = new Seller();
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				seller.setBirthDate(rs.getDate("BirthDate"));
-				seller.setDepartment(dep);
-				
+				Department dep = insantiateDepartment(rs);			
+				Seller seller = insantiateSeller(rs,dep);				
 				return seller;
 			}
 			return null;
@@ -84,6 +74,25 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 		}
+	}
+
+	private Seller insantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setDepartment(dep);
+		return seller;
+	}
+
+	private Department insantiateDepartment(ResultSet rs) throws SQLException {
+		
+		Department dep =new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
