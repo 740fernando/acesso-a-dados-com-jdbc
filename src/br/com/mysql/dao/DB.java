5033701cf1891bee28818e23fb4323d28d175f44
Fernando <fernando.luiz92@hotmail.com>
2022-01-23 11:23:54 -0300

    Sem logica de transacao

--- src/br/com/mysql/dao/DB.java
+++ src/br/com/mysql/dao/DB.java
@@ -14,56 +14,58 @@ import java.text.SimpleDateFormat;
 import java.util.Properties;
 
 public class DB {
-	
-	private static Connection conn=null; // conexão
-	private static Statement st = null; //consulta
+
+	private static Connection conn = null; // conexão
+	private static Statement st = null; // consulta
 	private static ResultSet rs = null; // recebe o resultado
-	
-	// método responsável por conectar com o banco de dados . Conectar no banco de dados em jdbc é instanciar um objeto do tipo "Connection"
+
+	// método responsável por conectar com o banco de dados . Conectar no banco de
+	// dados em jdbc é instanciar um objeto do tipo "Connection"
 	public static Connection getConnection() {
-		if(conn==null) {
+		if (conn == null) {
 			try {
-				Properties props= loadProperties();
-				String url= props.getProperty("dburl");
-				conn = DriverManager.getConnection(url,props);
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
 			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
-			}		
+			}
 		}
 		return conn;
 	}
-	
-	//fecha a conexão
+
+	// fecha a conexão
 	public static void closeConnection() {
-		if(conn!=null) {
+		if (conn != null) {
 			try {
 				conn.close();
-			}
-			catch(SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
-	
-	//método estático auxiliar -- carrega as propriedades do banco de dados retornando esses dados no objeto props
+
+	// método estático auxiliar -- carrega as propriedades do banco de dados
+	// retornando esses dados no objeto props
 	private static Properties loadProperties() {
-		
-		try(FileInputStream fs = new FileInputStream("db.properties")){
-			Properties props= new Properties();
-			props.load(fs);// load- faz a leitura do arquivo properties apontato pelo Input Stream fs e guardar os dados dentro do objeto props 
+
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);// load- faz a leitura do arquivo properties apontato pelo Input Stream fs e
+							// guardar os dados dentro do objeto props
 			return props;
-		}
-		catch(IOException e) {
+		} catch (IOException e) {
 			throw new DbException(e.getMessage()); // exceão personalizada
 		}
 	}
-public static void inserirDadosComRetornoChave() {
-		
+
+	public static void inserirDadosComRetornoChave() {
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-		
+
 //			st = conn.prepareStatement(
 //										" INSERT INTO seller "
 //										+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
@@ -76,149 +78,172 @@ public static void inserirDadosComRetornoChave() {
 //			st.setDate(3, new Date(sdf.parse("22/04/1985").getTime()));
 //			st.setDouble(4, 3000.0);
 //			st.setInt(5, 4);
-			
-			st= conn.prepareStatement(
-					"insert into department (Name) values ('D1'),('D2')",
+
+			st = conn.prepareStatement("insert into department (Name) values ('D1'),('D2')",
 					Statement.RETURN_GENERATED_KEYS);
-			
+
 			int rowsAffected = st.executeUpdate();
 			int queryTimout = st.getQueryTimeout();
-			
+
 			if (rowsAffected > 0) {
 				rs = st.getGeneratedKeys(); // Função retorna objeto do tipo resultSet, pode ter mais valores;
-				while(rs.next()) {
-					int id = rs.getInt(1); // O valor passado no parametro é 1, porque o rs vai conter apenas um valor para ser extraído
-					System.out.println("Done! Id = "+id);
-				}				
+				while (rs.next()) {
+					int id = rs.getInt(1); // O valor passado no parametro é 1, porque o rs vai conter apenas um valor
+											// para ser extraído
+					System.out.println("Done! Id = " + id);
+				}
 			}
-			
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			System.out.println(e);
 //		}catch(ParseException e) {
 //			System.out.println(e);	
-		}finally {
+		} finally {
 			closeStatement(st);
 			closeConnection();
 		}
 	}
+
 	public static void inserirDados() {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-			String sql = "INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+" VALUES"
+			String sql = "INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId)" + " VALUES"
 					+ "(?,?,?,?,?)";
 			st = conn.prepareStatement(sql);
-			
+
 			st.setString(1, "Carl Purple");
 			st.setString(2, "carl@gmail.com");
 			st.setDate(3, new Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
-			
+
 			int rowsAffected = st.executeUpdate();
 			int queryTimout = st.getQueryTimeout();
-			
-			System.out.println("Rows Affected : "+ rowsAffected+"\nqueryTimout "+ queryTimout);
-			
-		}catch(SQLException e) {
+
+			System.out.println("Rows Affected : " + rowsAffected + "\nqueryTimout " + queryTimout);
+
+		} catch (SQLException e) {
 			System.out.println(e);
-		}catch(ParseException e) {
-			System.out.println(e);	
-		}finally {
+		} catch (ParseException e) {
+			System.out.println(e);
+		} finally {
 			closeStatement(st);
 			closeConnection();
 		}
 	}
-	public static void atualizarDados() {
-		
-		PreparedStatement st =null;
-		
-		try {
-			conn= DB.getConnection(); //conecta o banco de dados
 
-			st = conn.prepareStatement("UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
-					+"WHERE "
-					+"(DepartmentId= ?)");
-			
+	public static void atualizarDados() {
+
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection(); // conecta o banco de dados
+
+			st = conn.prepareStatement(
+					"UPDATE seller " + "SET BaseSalary = BaseSalary + ? " + "WHERE " + "(DepartmentId= ?)");
+
 			st.setDouble(1, 200.0);
-			st.setInt(2,2);
-			
+			st.setInt(2, 2);
+
 			int rowsAffected = st.executeUpdate(); // Qtd de linhas que foram afetadas.
-			
-			System.out.println("Done! Rows affected : "+rowsAffected);
-			
-		}catch(SQLException e) {
+
+			System.out.println("Done! Rows affected : " + rowsAffected);
+
+		} catch (SQLException e) {
 			System.out.println(e);
-		}finally {
+		} finally {
 			closeStatement(st);
 			closeConnection();
-		}	
+		}
 	}
+
 	public static void recuperarDados() {
 
 		try {
-			conn= DB.getConnection(); //conecta o banco de dados
+			conn = DB.getConnection(); // conecta o banco de dados
 
-			st = conn.createStatement(); //instanciei um objeto Statement
+			st = conn.createStatement(); // instanciei um objeto Statement
 
 			rs = st.executeQuery("select * from department");
 
-			while(rs.next()) {
-				System.out.println(rs.getInt("id")+", "+rs.getString("Name"));
+			while (rs.next()) {
+				System.out.println(rs.getInt("id") + ", " + rs.getString("Name"));
 			}
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			System.out.println(e);
-		}finally {
+		} finally {
 			closeStatement(st);
 			closeResultSet(rs);
 			closeConnection();
-		}	
+		}
 	}
+
 	public static void deletarDados() {
-		
+
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-			
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+"WHERE "
-					+"Id = ? ");
-			
-		st.setInt(1, 2);
-			
-		int rowsAffected = st.executeUpdate();
-		
-		System.out.println("Done! Rows affected: "+rowsAffected);
-				
-					
-		}catch (SQLException e) {
+
+			st = conn.prepareStatement("DELETE FROM department " + "WHERE " + "Id = ? ");
+
+			st.setInt(1, 2);
+
+			int rowsAffected = st.executeUpdate();
+
+			System.out.println("Done! Rows affected: " + rowsAffected);
+
+		} catch (SQLException e) {
 			throw new DbIntegrityException(e.getMessage());
-		}finally {
+		} finally {
 			closeStatement(st);
 			closeConnection();
 		}
 	}
+	public static void transacoes() {
+				
+		try {			
+			conn = getConnection();
+			
+			
+			st = conn.createStatement();
+			
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1 ");
+			
+			int x = 1; 
+			if(x<2) {
+				throw new SQLException("Fake error");
+			}
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2 ");
+
+			
+			System.out.println("rows1 "+rows1);
+			System.out.println("rows2 "+rows2);			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			closeConnection();
+			closeStatement(st);
+		}
+	}
 	public static void closeStatement(Statement st) {
-		if(st != null) {
+		if (st != null) {
 			try {
 				st.close();
-			} catch(SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
 
-	public static void closeResultSet (ResultSet rs) {
-		if(rs != null) {
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
 			try {
 				rs.close();
-			} catch(SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
