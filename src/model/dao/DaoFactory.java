299939a26b9a3cfb88bbeed0de9d5c57d5978ed1
Fernando <fernando.luiz92@hotmail.com>
2022-01-25 15:32:43 -0300

    findAll Department

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -9,9 +9,9 @@ import model.dao.impl.SellerDaoJDBC;
  * createSellerDao - return @ SellerDaoJDBC()
  * 
  * 
- * Dessa forma, a classe DaoFactory vai expor o tipo da interface, 
- * mas internamente vai instanciar a implementacao.
- * É uma forma de fazer a injeção de depedencia, sem explicitar a implementação.
+ * Dessa forma, a classe DaoFactory vai expor o tipo da interface, mas
+ * internamente vai instanciar a implementacao. É uma forma de fazer a injeção
+ * de depedencia, sem explicitar a implementação.
  * 
  * 
  * @author Fernando
@@ -23,8 +23,9 @@ public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
+
 	public static DepartmentDao createDepartmentDao() {
 		return new DepartmentDaoJDBC(DB.getConnection());
 	}
-	
+
 }
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -5,10 +5,14 @@ import java.util.List;
 import model.entitites.Department;
 
 public interface DepartmentDao {
-	
+
 	void insert(Department obj);
+
 	void update(Department obj);
+
 	void deleteById(Integer id);
+
 	Department findById(Integer id);
+
 	List<Department> findAldd();
 }
