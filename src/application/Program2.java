eb3ab37da057babd17818f66782a33b78b986e5f
Fernando <fernando.luiz92@hotmail.com>
2022-01-25 10:21:36 -0300

    Insert Implementation Department

---
+++ src/application/Program2.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entitites.Department;
+
+public class Program2 {
+	public static void main(String[] args) {
+
+		System.out.println("============= Teste 1 : Department insert() =============");
+
+		Department dep = new Department();
+		DepartmentDao depDao = DaoFactory.createDepartmentDao();
+		dep.setName("RH");
+		depDao.insert(dep);
+		System.out.println("Inserido novo dep id : "+dep.getId()+" nome : "+dep.getName());
+		
+	}
+}
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,84 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entitites.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+	private Connection conn;
+	
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn= conn;
+	}
+	
+	@Override
+	public void insert(Department department) {
+		
+		PreparedStatement st = null;
+
+		try {
+
+			StringBuilder query = new StringBuilder();
+			query.append("INSERT INTO department ");
+			query.append("(Name) ");
+			query.append("VALUES ");
+			query.append("(?)");
+
+			st = conn.prepareStatement(query.toString(), Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, department.getName());
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					department.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Erro inesperado, nenhuma linha foi afetada");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
+	}
+
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAldd() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
